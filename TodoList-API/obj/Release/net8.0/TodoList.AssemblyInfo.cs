78b84d26739dbba425a5fc1b0fa2202795dfa2f1
Maral katebi <marall@live.com>
2025-11-13 11:28:38 +0330

    pushing changes

--- TodoList-API/obj/Release/net8.0/TodoList.AssemblyInfo.cs
+++ TodoList-API/obj/Release/net8.0/TodoList.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+963abc1d75b1d00386ce00a87ef30ced8ddaf92d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
